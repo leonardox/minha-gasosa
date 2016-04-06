3a0de53aee67dd72e3dd4e4bb5e0085b354636ae
viniciusjs <vinicius.silva@ccc.ufcg.edu.br>
2016-04-06 01:57:40 -0300

    Mecanismo para utilizar a potência caso a listagem não seja usada task#3986

---
+++ aplicacao/app/src/main/java/com/minhagasosa/preferences/MinhaGasosaPreference.java
@@ -0,0 +1,47 @@
+package com.minhagasosa.preferences;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.minhagasosa.R;
+
+/**
+ * Created by Vinicius Silva on 06/04/2016.
+ */
+public class MinhaGasosaPreference {
+
+    final private static String PREFERENCE = "com.minhagasosa.preference";
+
+
+    private static SharedPreferences.Editor getSharedPreference(Context context){
+        SharedPreferences preferences = context.getSharedPreferences(PREFERENCE,
+                Context.MODE_PRIVATE);
+        return preferences.edit();
+    }
+
+    public static void putWithPotency(boolean withPotency, Context context){
+        SharedPreferences.Editor editor = getSharedPreference(context);
+        editor.putBoolean(context.getString(R.string.com_potencia),withPotency);
+        editor.commit();
+    }
+
+    public static void putPotency(int potency, Context context){
+        SharedPreferences.Editor editor = getSharedPreference(context);
+        editor.putInt(context.getString(R.string.sharedPotencia), potency);
+        editor.commit();
+    }
+
+    public static boolean getWithPotency(Context context){
+        SharedPreferences preferences = context.getSharedPreferences(PREFERENCE,
+                Context.MODE_PRIVATE);
+        return preferences.getBoolean(context.getString(R.string.com_potencia),false);
+
+    }
+    public static int getPotency(Context context){
+        final SharedPreferences preferences =  context.getSharedPreferences(PREFERENCE,
+                Context.MODE_PRIVATE);
+        return preferences.getInt(context.getString(R.string.sharedPotencia), -1);
+    }
+
+}
