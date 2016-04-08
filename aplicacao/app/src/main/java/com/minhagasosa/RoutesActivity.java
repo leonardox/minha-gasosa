0f6b2cba9e2cbecff868ba3c7b7e749ee6288fcc
aparecido silvano <silvanoalbuquerque@gmail.com>
2016-04-08 01:50:50 -0300

    cria ui para adição de rotas no sistema.

--- aplicacao/app/src/main/java/com/minhagasosa/RoutesActivity.java
+++ aplicacao/app/src/main/java/com/minhagasosa/RoutesActivity.java
@@ -1,51 +1,46 @@
 package com.minhagasosa;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+public class RoutesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_routes);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this, RoutesActivity.class);
-                startActivity(intent);
-                //Toast.makeText(HomeActivity.this, "Ação de adicionar rota aqui...", Toast.LENGTH_SHORT).show();
-            }
-        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final TextInputLayout distanceWrapper = (TextInputLayout) findViewById(R.id.distance_wrapper);
+        final TextInputLayout timesRouteWrapper = (TextInputLayout) findViewById(R.id.times_routes_wraper);
+
+        distanceWrapper.setHint(getResources().getString(R.string.dist_km));
+        timesRouteWrapper.setHint(getResources().getString(R.string.times_routes_week));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-
+        inflater.inflate(R.menu.distance_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.set_car) {
-            Intent i = new Intent(this, MainActivity.class);
-            i.putExtra("fromHome", true);
-            startActivity(i);
+        if (item.getItemId() == R.id.done_route) {
+            Snackbar.make(getCurrentFocus(), "Falta implementar o salvar da rota", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
         }
         return super.onOptionsItemSelected(item);
     }
