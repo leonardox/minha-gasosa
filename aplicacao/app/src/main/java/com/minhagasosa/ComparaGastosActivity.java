8ec8f499b23a85c0e9856920c26d6c6785939d46
aparecido silvano <silvanoalbuquerque@gmail.com>
2016-05-05 08:29:54 -0300

    começa implementação da UI para tela de comparação de gastos

---
+++ aplicacao/app/src/main/java/com/minhagasosa/ComparaGastosActivity.java
@@ -0,0 +1,13 @@
+package com.minhagasosa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ComparaGastosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compara_gastos);
+    }
+}
