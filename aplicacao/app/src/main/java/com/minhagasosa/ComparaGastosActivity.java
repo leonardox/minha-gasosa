32463f6abc27441fb3baa5e8f8d0406aaa8efe51
aparecido silvano <silvanoalbuquerque@gmail.com>
2016-05-11 06:20:21 -0300

    inicia as instâncias do gráfico para o mês atual e para o mês anterior.

--- aplicacao/app/src/main/java/com/minhagasosa/ComparaGastosActivity.java
+++ aplicacao/app/src/main/java/com/minhagasosa/ComparaGastosActivity.java
@@ -6,15 +6,22 @@ import android.os.Bundle;
 import com.github.mikephil.charting.charts.PieChart;
 
 public class ComparaGastosActivity extends AppCompatActivity {
-    private ChartView chartView;
+    private ChartView chartViewCurrentMonth;
+    private ChartView chartViewLastMonth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compara_gastos);
 
-        PieChart pieChart = (PieChart) findViewById(R.id.chart_current_month);
-        chartView = new ChartView(this, pieChart);
-        chartView.iniciaDistancias();
+        // ele já sabe construir o gráfico do mês atual.
+        PieChart pieChartCurrentMonth = (PieChart) findViewById(R.id.chart_current_month);
+        chartViewCurrentMonth = new ChartView(this, pieChartCurrentMonth);
+        chartViewCurrentMonth.iniciaDistancias();
+
+        // todo fazer o chatViewLastMonth saber montar o gráfico para o mês passado.
+        PieChart pieChartLastMonth = (PieChart) findViewById(R.id.chart_last_month);
+        chartViewLastMonth = new ChartView(this, pieChartLastMonth);
+        chartViewLastMonth.iniciaDistancias();
     }
 }
