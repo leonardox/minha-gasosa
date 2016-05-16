ce8519e375d711cfda8d4f1c7ab4efe69244dfac
aparecido silvano <silvanoalbuquerque@gmail.com>
2016-05-16 20:45:15 -0300

    altera horário default da notificação.

--- aplicacao/app/src/main/java/com/minhagasosa/HomeActivity.java
+++ aplicacao/app/src/main/java/com/minhagasosa/HomeActivity.java
@@ -257,8 +257,8 @@ public class HomeActivity extends AppCompatActivity {
      */
     private void startTheNotificationLauncher() {
         Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, 20);
-        calendar.set(Calendar.MINUTE, 41);
+        calendar.set(Calendar.HOUR_OF_DAY, 9);
+        calendar.set(Calendar.MINUTE, 0);
         calendar.set(Calendar.SECOND, 0);
 
         Intent intent = new Intent(HomeActivity.this, MyReceiver.class);
