82d20fb7836683b46f499113c32c1ffb0714fc06
alexnum <alexnum@gmail.com>
2016-05-24 21:04:38 -0300

    Showing month routes

--- aplicacao/app/src/main/java/com/minhagasosa/ListRoutesActivity.java
+++ aplicacao/app/src/main/java/com/minhagasosa/ListRoutesActivity.java
@@ -11,6 +11,9 @@ import com.minhagasosa.dao.DaoSession;
 import com.minhagasosa.dao.Rota;
 import com.minhagasosa.dao.RotaDao;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class ListRoutesActivity extends AppCompatActivity {
@@ -25,9 +28,16 @@ public class ListRoutesActivity extends AppCompatActivity {
         DaoSession session = daoMaster.newSession();
         RotaDao rDao = session.getRotaDao();
         List<Rota> rotas  =rDao.loadAll();
-
+        Date today = new Date();
+        List<Rota> rotasMes = new ArrayList<Rota>();
+        for (Rota r:
+             rotas) {
+            if(today.getMonth() == r.getData().getMonth() && today.getYear() == r.getData().getYear()){
+                rotasMes.add(r);
+            }
+        }
         ListView lv = (ListView) findViewById(R.id.lvRotas);
-        ListAdapter la = new RotaAdapter(this, R.layout.route_adapter, rotas);
+        ListAdapter la = new RotaAdapter(this, R.layout.route_adapter, rotasMes);
         lv.setAdapter(la);
     }
 }
