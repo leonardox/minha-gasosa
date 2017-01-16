b081267a1942aa1bc803445ab88dfd4fd191a42d
Andre Yuri <andrecg16@gmail.com>
2017-01-15 23:31:34 -0300

    Personalizando pagina do posto

--- aplicacao/app/src/main/java/com/minhagasosa/activites/maps/GasStationActivity.java
+++ aplicacao/app/src/main/java/com/minhagasosa/activites/maps/GasStationActivity.java
@@ -2,6 +2,7 @@ package com.minhagasosa.activites.maps;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -27,6 +28,10 @@ public class GasStationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_gas_station);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        collapsingToolbar.setTitle(gas.getName());
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
